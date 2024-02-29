35cc7cfa1bde8125fe6cd8bfcfe34978152dac9d
Yerzhan Amirkhanov <yerzhanamirkhanov@gmail.com>
2024-02-29 19:08:25 +0600

    ListView for Tests and Diagnostic created

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import IndexView
+from main.views import *
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+
+    path('diagnostics/', DiagnosticCategoryListView.as_view(), name='diagnostics'),
+    path('diagnostics/category_create/', DiagnosticCategoryCreateView.as_view(), name='diagnostic_category_create'),
+    path('diagnostics/create/', DiagnosticCreateView.as_view(), name='diagnostic_create'),
+
+    path('tests/', TestCategoryListView.as_view(), name='tests'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
+from django.views.generic import ListView, CreateView
+
+from main.forms import DiagnosticForm, DiagnosticCategoryForm
+from main.models import Diagnostic, DiagnosticCategory, TestCategory
 
 
 class IndexView(generic.View):
@@ -14,3 +20,28 @@ class IndexView(generic.View):
             # 'object_list': three_random_articles,
         }
         return render(request, 'main/index.html', context)
+
+
+class DiagnosticCategoryListView(ListView):
+    model = DiagnosticCategory
+
+
+class DiagnosticCategoryCreateView(LoginRequiredMixin, CreateView):
+    model = DiagnosticCategory
+    form_class = DiagnosticCategoryForm
+
+    def get_success_url(self):
+        return reverse('main:diagnostics', args=[self.object.pk])
+
+
+class DiagnosticListView(ListView):
+    model = Diagnostic
+
+
+class DiagnosticCreateView(LoginRequiredMixin, CreateView):
+    model = Diagnostic
+    form_class = DiagnosticForm
+
+
+class TestCategoryListView(ListView):
+    model = TestCategory
